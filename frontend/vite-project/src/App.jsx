ed9bbf56903663a2d7f909aa5760edbcfad30925
I-Sharmilabanu <banu69136@gmail.com>
2025-12-25 09:24:27 +0530

    Change email API endpoint to production URL

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     formData.append("emailList", JSON.stringify(emailList));
     formData.append("file", selectedFile);
 
-    axios.post("http://localhost:5000/sendemail", formData)
+    axios.post("https://bulkmail-wibk.onrender.com", formData)
       .then(res => {
         const { success, failedEmails: failed } = res.data;
         setFailedEmails(failed || []);
